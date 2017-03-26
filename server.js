afaa5d2912ee7947b2c7206bf47ad8329cc5cd12
Arpit Goyal <arpit.go4@gmail.com>
2017-03-26 23:00:46 +0530

    PORT env var added for server

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(process.env.$PORT || 8080, function(err) {
+app.listen(process.env.PORT || 8080, function(err) {
   if (err) {
     return console.error(err);
   }
 
-  console.log('Listening at http://localhost:8080/', process.env.$PORT);
+  console.log('Listening at http://localhost:8080/', process.env.PORT);
 });
