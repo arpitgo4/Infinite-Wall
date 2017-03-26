7deb7f9ff899553e4d7f58170322daa229002f97
Arpit Goyal <arpit.go4@gmail.com>
2017-03-26 15:57:43 +0530

    Post component refactored to stateless functional component

--- client/components/post/post.component.js
+++ client/components/post/post.component.js
@@ -6,31 +6,23 @@ import PostHeader from './header/header.post.component';
 import PostBody from './body/body.post.component';
 import PostFooter from './footer/footer.post.component';
 
-export default class Post extends React.Component {
+const Post = ({ post }, { store }) => {
 
-	componentWillMount() {
-		this.store = this.context.store;
-		console.log('post store', this.context.store);
-	}
-
-	render() {
-
-		const { id, title, timePosted, subtitle,
+	const { id, title, timePosted, subtitle,
 				 image, summary, summaryTitle, source, 
-				 likes, comments, shares } = this.props.post;
+				 likes, comments, shares } = post;
 
-		return (
-			<div style={stylesheet.postContainer} id="postContainer">
-				<PostHeader id={id} title={title} timePosted={timePosted} subtitle={subtitle} />
+	return (
+		<div style={stylesheet.postContainer} id="postContainer">
+				<PostHeader title={title} timePosted={timePosted} subtitle={subtitle} />
 				<PostBody image={image} summary={summary} summaryTitle={summaryTitle} source={source} />
 				<PostFooter likes={likes} comments={comments} shares={shares}
-							likeHandler={() => this.store.dispatch({ type: 'POST_LIKED', id })} 
-							shareHandler={() => this.store.dispatch({ type: 'SHARE_CLICKED', id })} 
-							commentHandler={() => this.store.dispatch({ type: 'COMMENT_CLICKED', id })} />
-			</div>
-		);
-	}
-}
+							likeHandler={() => store.dispatch({ type: 'POST_LIKED', id })} 
+							shareHandler={() => store.dispatch({ type: 'SHARE_CLICKED', id })} 
+							commentHandler={() => store.dispatch({ type: 'COMMENT_CLICKED', id })} />
+		</div>
+	)
+};
 
 Post.contextTypes = {
 	store: React.PropTypes.object
@@ -41,3 +33,5 @@ const stylesheet = {
 		backgroundColor: 'white'
 	}
 };
+
+export default Post;
